a9fa16ac39c7cad7e767d382d7306a610bf32b3c
jthom143-Blaustein <jthom143@jhu.edu>
2014-10-23 14:57:12 -0400

    Changed trend period to be 25 years

--- wind_trend_figures.m
+++ wind_trend_figures.m
@@ -3,57 +3,109 @@ figure(1)
 plot(can_esm2_jet_xi, can_esm2_jet_f, 'r')
 hold on
 plot(cnrm_cm5_jet_xi      , cnrm_cm5_jet_f        , 'k')
-plot(cnrm_cm52_jet_xi     , cnrm_cm52_jet_f       , 'm')
-plot(gfdl_esm2g_jet_xi    , gfdl_esm2g_jet_f      , 'c' )
 plot(gfdl_esm2m_jet_xi    , gfdl_esm2m_jet_f      , 'g' )
-hR1 = vline(jet_u850_trend_30, '-k');
-legend('CanESM2', 'CNRM CM5', 'CNRM CM5-2', 'GFDL ESM2G', 'GFDL ESM2M')
+plot(mpi_esm_lr_jet_xi    , mpi_esm_lr_jet_f      , 'b' )
+hR1 = vline(jet_u850_trend_30, '-g');
+legend('CanESM2', 'CNRM CM5', 'GFDL ESM2M', 'MPI ESM LR')
 xlabel('Jet Trend (m/s/30 years)')
 ylabel('Probability Density')
 
 
  
 figure(8)
-subplot(2,3,1)
+subplot(2,2,1)
 plot(can_esm2_jet_xi, can_esm2_jet_f)
 title('CanESM2', 'fontsize', 12)
 ylim([0 1.5])
-hR1 = vline(jet_u850_trend_30, '-k');
-hHist = vline(can_esm2_jet_trend_30, 'r');
+hR1 = vline(jet_u850_trend_30, '-g');
+hHist = vline(can_esm2_jet_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+ylabel('Probability Density')
 
 
-subplot(2,3,2)
+subplot(2,2,2)
 plot(cnrm_cm5_jet_xi      , cnrm_cm5_jet_f )
 title('CNRM CM5', 'fontsize', 12)
 ylim([0 1.5])
-hR1 = vline(jet_u850_trend_30, '-k');
-hHist = vline(cnrm_cm5_jet_trend_30, 'r');
+hR1 = vline(jet_u850_trend_30, '-g');
+hHist = vline(cnrm_cm5_jet_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
 
 
 
-subplot(2,3,3)
-plot(cnrm_cm52_jet_xi     , cnrm_cm52_jet_f )
-title('CNRM CM5-2', 'fontsize', 12)
-ylim([0 1.5])
-hR1 = vline(jet_u850_trend_30, '-k');
-hHist = vline(cnrm_cm52_jet_trend_30, 'r');
-
-
-
-subplot(2,3,4)
-plot(gfdl_esm2g_jet_xi    , gfdl_esm2g_jet_f)
-title('GFDL ESM2G', 'fontsize', 12)
-ylim([0 1.5])
-hR1 = vline(jet_u850_trend_30, '-k');
-hHist = vline(gfdl_esm2g_jet_trend_30, 'r');
-
-
-
-subplot(2,3,5)
+subplot(2,2,3)
 plot(gfdl_esm2m_jet_xi    , gfdl_esm2m_jet_f )
 title('GFDL ESM2M', 'fontsize', 12)
 ylim([0 1.5])
-hR1 = vline(jet_u850_trend_30, '-k');
-hHist = vline(gfdl_esm2m_jet_trend_30, 'r');
+hR1 = vline(jet_u850_trend_30, '-g');
+hHist = vline(gfdl_esm2m_jet_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+
+
+
+
+subplot(2,2,4)
+plot(mpi_esm_lr_jet_xi, mpi_esm_lr_jet_f)
+title('MPI ESM LR', 'fontsize', 12)
+ylim([0 1.5])
+hR1 = vline(jet_u850_trend_30, '-g');
+hHist = vline(mpi_esm_lr_jet_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+xlabel('25 year trend (m/s/25yrs)')
+
+
+
+ 
+figure(9)
+subplot(2,2,1)
+plot(can_esm2_jet_loc_xi, can_esm2_jet_loc_f)
+title('CanESM2', 'fontsize', 12)
+%ylim([0 1.5])
+hR1 = vline(lat_u850_trend_30, '-g');
+hHist = vline(can_esm2_jet_loc_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+ylabel('Probability Density')
+
+
+subplot(2,2,2)
+plot(cnrm_cm5_jet_loc_xi      , cnrm_cm5_jet_loc_f )
+title('CNRM CM5', 'fontsize', 12)
+%ylim([0 1.5])
+hR1 = vline(lat_u850_trend_30, '-g');
+hHist = vline(cnrm_cm5_jet_loc_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+
+
+
+subplot(2,2,3)
+plot(gfdl_esm2m_jet_loc_xi    , gfdl_esm2m_jet_loc_f )
+title('GFDL ESM2M', 'fontsize', 12)
+%ylim([0 1.5])
+hR1 = vline(lat_u850_trend_30, '-g');
+hHist = vline(gfdl_esm2m_jet_loc_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+
+
+
+
+subplot(2,2,4)
+plot(mpi_esm_lr_jet_loc_xi, mpi_esm_lr_jet_loc_f)
+title('MPI ESM LR', 'fontsize', 12)
+%ylim([0 1.5])
+hR1 = vline(lat_u850_trend_30, '-g');
+hHist = vline(mpi_esm_lr_jet_loc_trend_30_u850, 'r');
+set(hR1,  'color', [0, 0.8, 0], 'linewidth', 1.5);
+set(hHist, 'linewidth', 1.5);
+xlabel('25 year trend (degrees/25yrs)')
+
+
+
 
 
